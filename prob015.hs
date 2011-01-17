441a191fa844829dbf1a1564ca430e1511922ab6
andreoss <andreoss@sdf.org>
2011-01-17 09:58:14 +0100

    Problem 15

---
+++ prob015.hs
@@ -0,0 +1,6 @@
+factorial n   = product [1..n]
+binomial  n k = (factorial n) / ((factorial k) * (factorial (n - k)))
+latice    n   = binomial (n * 2) n
+
+answer = round $ latice 20
+main   = do print answer
