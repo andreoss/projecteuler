6f7938281d6fdf2acc387a783de69e9b7cc659e5
andreoss <andreoss@sdf.org>
2011-03-05 07:05:41 +0100

    problem 42

---
+++ prob042.hs
@@ -0,0 +1,24 @@
+import Data.List.Split
+loadWords :: IO [String]
+loadWords = do
+  allWords <- readFile "p042_words.txt"
+  let quotedWords = splitOn "," allWords
+  return $ map (filter (/= '"')) quotedWords
+
+triagleNum n = (n * (n + 1)) `div` 2
+
+wordValue [] = 0
+wordValue (x:xs) = (ch x) + wordValue xs
+                   where
+                     ch c = (fromEnum c) - (fromEnum 'A') + 1
+
+triagles = triagles' 1
+  where triagles' x = (triagleNum x) : (triagles' (x+1))
+
+triaglesTo n = takeWhile (<= n) triagles
+isTriangle :: Int -> Bool
+isTriangle n = n == last (triaglesTo n)
+
+main = do
+  words <- loadWords
+  let answer = length $ filter isTriangle (map wordValue words) in print answer
