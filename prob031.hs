a2f48f1fd3255c576c9c6477e70e0387a86c8fd2
andreoss <andreoss@sdf.org>
2011-03-26 05:45:03 +0100

    problem 31

---
+++ prob031.hs
@@ -0,0 +1,11 @@
+
+
+solve    []     s         = 0
+
+solve    (c:ct) 0         = 1
+
+solve    (c:ct) s | c > s = solve ct s
+
+solve cs@(c:ct) s         = (solve cs (s - c)) + (solve ct s)
+
+answer = solve [200,100,50,20,10,5,2,1] 200
