9fd8944322570c4bc8411f0fad6f06e387a1d744
andreoss <andreoss@sdf.org>
2011-04-13 07:38:48 +0100

    prob 49

---
+++ prob049.hs
@@ -0,0 +1,36 @@
+
+import qualified Data.Set as Set
+
+digits :: Int -> [Int]
+digits = (map negate) . (map (((-) (fromEnum '0') ) . fromEnum)) . show 
+
+isPermute a b = Set.fromList ad == Set.fromList bd
+                where
+                  ad = digits a
+                  bd = digits b
+
+isPrime :: Integral a => a -> Bool
+isPrime n
+  | n == 1     = False
+  | n == 2     = True
+  | even n    = False
+  | otherwise = null $ filter (\ x -> rem n x == 0) [3 .. l]
+  		where l = round $ sqrt $ fromIntegral n
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _      = [[]]
+combinations _ []     = []
+combinations k (x:xs) = map (x:) (combinations (k - 1) xs) ++
+                          combinations k xs
+
+primes   = filter isPrime [1000..9999]
+
+isGood n = filter (\[a,b,c] -> a == n && b == a + 3330 && c == b + 3330 ) $ combinations 3 $ filter (isPermute n) primes
+
+result   = map head $ filter (not . null) $ map isGood primes
+
+answer   = foldr (++) "" $ map show $ result !! 1
+
+main     = do print answer
+
+
