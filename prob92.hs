88fbdc40060e5f0f719e9089c631a1c16e6a80b5
andreoss <andreoss@sdf.org>
2011-05-06 05:55:04 +0100

    Problem 92

---
+++ prob92.hs
@@ -0,0 +1,43 @@
+import Control.Monad.State
+import qualified Data.IntMap as M
+
+digits :: Int -> [Int]
+digits = (map negate) . 
+  (map (((-) (fromEnum '0') ) . fromEnum)) . show 
+
+
+square :: Int -> Int
+square x = x *x 
+
+step :: Int -> Int
+step = sum . (map square) . digits
+{-
+terminator :: Int -> Int
+terminator n
+  | n' == 1 = 1
+  | n' == 89 = 89
+  | otherwise = terminator n'
+  where n' = step n
+-}
+
+terminator' :: Int -> State (M.IntMap Int) Int
+terminator' n = do
+  m <- get
+  if M.member n m
+     then return (m M.! n)
+    else do t <- terminator (step n)
+            modify (M.insert n t)
+            return t
+
+terminator :: Int -> State (M.IntMap Int) Int
+terminator n =
+  if (n == 1) || (n == 89)
+  then
+    return n
+  else terminator' n
+
+terminators :: [Int] -> State (M.IntMap Int) [Int]
+terminators = mapM terminator
+
+
+main = print $ length $ filter (==89) $ evalState (terminators [1..10000000]) M.empty 
