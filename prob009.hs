aae63f4969df093fec22afa889aebc1b51e47a41
andreoss <andreoss@sdf.org>
2011-06-10 04:49:55 +0100

    rewrite

--- prob009.hs
+++ prob009.hs
@@ -1,2 +1,6 @@
-answer = product $ head $ [ [a, b, c] | a <- [1..1000], b <- [1..a], let c=1000-a-b, a^2+b^2==c^2 ]
-main   = do print answer
+answer = product $ head [ [a, b, c]
+          | a <- [1 .. 1000],
+            b <- [1 .. a],
+            let c = 1000 - a - b,
+            a ^ 2 + b ^ 2 == c ^ 2 ]
+main   = print answer
