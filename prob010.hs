699dcc4529c05721c3f2f3dc2a7eb50e71a500c7
andreoss <andreoss@sdf.org>
2011-06-14 14:12:15 +0100

    rewrite 10

--- prob010.hs
+++ prob010.hs
@@ -3,28 +3,27 @@ merge :: (Ord a) => [a] -> [a] -> [a]
 -- merge xs        []        = xs
 merge xs@(x:xt) ys@(y:yt) = 
   case compare x y of
-    LT -> x : (merge xt ys)
-    EQ -> x : (merge xt yt)
-    GT -> y : (merge xs yt)
-
+    LT -> x : merge xt ys
+    EQ -> x : merge xt yt
+    GT -> y : merge xs yt
 
 diff :: (Ord a) => [a] -> [a] -> [a]
 -- diff []         ys       =  ys
 -- diff xs         []       =  xs
 diff xs@(x:xt) ys@(y:yt) = 
   case compare x y of
-    LT -> x : (diff xt ys)
+    LT -> x : diff xt ys
     EQ -> diff xt yt
     GT -> diff xs yt
 
 primes, nonprimes :: [Integer]
-primes    = [2, 3, 5] ++ (diff [7, 9 ..] nonprimes) 
+primes    = [2, 3, 5] ++ diff [7, 9 ..] nonprimes
 nonprimes = foldr1 f $ map multiplicative $ tail primes
   where 
-    f (x:xt) ys      = x : (merge xt ys)
+    f (x:xt) ys      = x : merge xt ys
     multiplicative p = [ n * p | n <- [p, p + 2 ..]]
 
 
 answer = sum $ takeWhile (< 2000000) primes
 
-main   = do print answer
+main   = print answer
