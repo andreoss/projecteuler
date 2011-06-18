eca7ff62701684f5e29d7a6f62462529547fb381
andreoss <andreoss@sdf.org>
2011-06-18 12:10:15 +0100

    fixes 87

--- prob087.hs
+++ prob087.hs
@@ -1,28 +1,30 @@
 import qualified Data.Set as S
 import Data.Ord  (comparing)
 import Data.List (maximumBy)
+
 merge :: (Ord a) => [a] -> [a] -> [a]
 merge []        ys        = ys
 merge xs        []        = xs
 merge xs@(x:xt) ys@(y:yt) = 
   case compare x y of
-    LT -> x : (merge xt ys)
-    EQ -> x : (merge xt yt)
-    GT -> y : (merge xs yt)
+    LT -> x : merge xt ys
+    EQ -> x : merge xt yt
+    GT -> y : merge xs yt
 
 diff :: (Ord a) => [a] -> [a] -> [a]
 diff []         ys       =  ys
 diff xs         []       =  xs
 diff xs@(x:xt) ys@(y:yt) = 
   case compare x y of
-    LT -> x : (diff xt ys)
+    LT -> x : diff xt ys
     EQ -> diff xt yt
     GT -> diff xs yt
 
+
 primes, nonprimes :: [Integer]
-primes    = [2, 3, 5] ++ (diff [7, 9 ..] nonprimes) 
+primes    = [2, 3, 5] ++ diff [7, 9 ..] nonprimes 
 nonprimes = foldr1 f $ map multiplicative $ tail primes
-  where f (x:xt) ys      = x : (merge xt ys)
+  where f (x:xt) ys      = x : merge xt ys
         multiplicative p = [ n * p | n <- [p, p + 2 ..]]
 
 limit        = 50 * 10^6
@@ -35,5 +37,4 @@ sums = [ a + b + c
          a + b + c < limit ]
 
 answer = S.size $ S.fromList sums
-main   = do print answer
-    
+main   = print answer
