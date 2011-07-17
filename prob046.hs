3af8c0a8468ed97695abee8a0f09a3a4357ce846
andreoss <andreoss@sdf.org>
2011-07-17 07:23:34 +0100

    fixes

--- prob046.hs
+++ prob046.hs
@@ -1,12 +1,11 @@
 square x = x * x
 double   = (* 2)
   
-isPrime :: Integral a => a -> Bool
 isPrime n
   | n == 1     = False
   | n == 2     = True
   | even n    = False
-  | otherwise = null $ filter (\ x -> rem n x == 0) [3 .. l]
+  | otherwise = (not  . any (\ x -> rem n x == 0)) [3 .. l]
   		where l = round $ sqrt $ fromIntegral n
 
 primes  = filter isPrime [1..]
@@ -28,4 +27,4 @@ result  = head
           $ filter odd [3..] 
           
 
-main    = do print result
+main    = print result
