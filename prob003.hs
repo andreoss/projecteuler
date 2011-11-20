bd268c6576f05fef7684acbdf7c88b9cff6c45b8
andreoss <andreoss@sdf.org>
2011-11-20 14:27:53 +0100

    Problem 3

---
+++ prob003.hs
@@ -0,0 +1,24 @@
+isPrime :: Integral a => a -> Bool
+isPrime n
+  | n == 1     = False
+  | n == 2     = True
+  | even n    = False
+  | otherwise = null $ filter (\ x -> rem n x == 0) [3 .. l]
+  		where l = round $ sqrt $ fromIntegral n
+
+nthPrime :: Integral a => Int -> a
+nthPrime = (!!) $ filter isPrime [1..]
+
+factors' :: Integral a => a -> Int -> [a] -> [a]
+factors' n primeN acc 
+  | n == 1 = acc
+  | otherwise = let prime = nthPrime primeN
+                in if rem n prime == 0 then
+                   factors' (quot n prime) primeN (prime:acc)
+                 else
+                   factors' n (primeN + 1) acc
+                   
+factors n = factors' n 0 []
+
+answer = maximum $ factors 600851475143 
+main   = do print answer
